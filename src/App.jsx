bb98d5a6611bc2e553b6556695af058f35775a56
Maya Sides <maya.sides@gmail.com>
2024-11-11 00:25:58 -0600

    the game needs to actually update after each play

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+export default function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -61,10 +61,18 @@ export default function Board() {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
