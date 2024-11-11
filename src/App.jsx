c9f55f6cd1af9dbaa6dd681b1fe752a51802d315
Maya Sides <maya.sides@gmail.com>
2024-11-11 00:29:40 -0600

    mapping over history in Game

--- src/App.jsx
+++ src/App.jsx
@@ -65,8 +65,27 @@ export default function Game() {
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(nextMove) {
     // TODO
   }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
 
   return (
     <div className="game">
