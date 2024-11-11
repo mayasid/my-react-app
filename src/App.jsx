bc2124d4fd18d9abe876ce9bb9a557746f5fcf56
Maya Sides <maya.sides@gmail.com>
2024-11-11 00:27:08 -0600

    Game updates board when the user clicks a square

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,7 @@ export default function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
