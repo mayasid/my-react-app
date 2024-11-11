7a246cbc9a9fb58aa463f1b6cfa2fcbe795d6fe2
Maya Sides <maya.sides@gmail.com>
2024-11-11 00:37:28 -0600

    render the currently selected move

--- src/App.jsx
+++ src/App.jsx
@@ -62,11 +62,12 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove) {
